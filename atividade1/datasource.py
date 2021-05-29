91bdf7bb78c2835af4ecc14a79c375f018f52d3f
André Roberto <andreroberto52@gmail.com>
2021-05-29 15:35:48 -0300

    Adicionando remoção de outliers

--- atividade1/datasource.py
+++ atividade1/datasource.py
@@ -1,7 +1,13 @@
+
 import urllib.request
 import pandas as pd
 import openpyxl
 import pandas_profiling
+import numpy as np
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 try:    
     f = open("2.9.xlsx")
@@ -10,9 +16,14 @@ except IOError:
     url = ("https://www.tesourotransparente.gov.br/ckan/dataset/fc2ea6ab-2130-4525-98f6-8bf04407f7fe/resource/ef6a250b-75bf-44c7-8c9f-8a746b6f469e/download/2.9.xlsx")    
     urllib.request.urlretrieve(url,'2.9.xlsx')
     
+    
 df = pd.read_excel("2.9.xlsx")
 print(df)
 print (df.dtypes)
+
+# Remove outliers
+# df = pd.DataFrame(np.random.rand(100, 3))
+# df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
 
 profile = pandas_profiling.ProfileReport(df)
 profile.to_file('report.html')
@@ -22,3 +33,5 @@ print (df.describe().iloc[:, :2])
 print ("Dados ausentes: " , df.isnull().sum())
 
 mask = df.isnull().any(axis=1)
+
+
